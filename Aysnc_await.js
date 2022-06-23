b467581e3e0a8d4c705653ca8db975dda3756158
unknown <kinekarpallavi21@gmail.com>
2022-06-23 19:30:33 +0530

    [Pallavi]Add:IntroduceES6 features

---
+++ Aysnc_await.js
@@ -0,0 +1,28 @@
+//Async Function
+async function f() {
+    console.log('Async function.');
+    return Promise.resolve(1);
+}
+
+f().then(function(result) {
+    console.log(result)
+});
+
+//JavaScript await Keyword
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+    resolve('Promise resolved')}, 4000); 
+});
+
+// async function
+async function asyncFunc() {
+
+    // wait until the promise resolves 
+    let result = await promise; 
+
+    console.log(result);
+    console.log('hello');
+}
+
+// calling the async function
+asyncFunc();
